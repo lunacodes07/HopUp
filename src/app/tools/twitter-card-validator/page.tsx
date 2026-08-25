907a5df1ab640c97b14315ae1239951a180cb87c
mdkamran <mohamedkamran04@gmail.com>
2026-08-26 00:44:54 +0530

    fix

--- src/app/tools/twitter-card-validator/page.tsx
+++ src/app/tools/twitter-card-validator/page.tsx
@@ -97,7 +97,7 @@ export default function TwitterCardValidator() {
           </button>
         </form>
 
-        {/* Error Messages */}
+        {/* Error Message */}
         <AnimatePresence>
           {error && (
             <motion.div
